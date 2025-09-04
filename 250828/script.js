d88c23a48eb8952b9092b141bced2d501461ef0a
kartia01 <kartia25142@gmail.com>
2025-09-04 17:34:47 +0900

    first

---
+++ 250828/script.js
@@ -0,0 +1,15 @@
+// alert('test')
+console.log('접속완료');
+
+function clickBtn(obj, money, name){
+    console.log('클릭완료');
+    console.log(obj.src);
+    console.log(money);
+    console.log(name);
+    obj.src = 'travel2.jpg';
+}
+
+// () = 행동을 취한다.
+// document.write('test');
+// console.log('test');
+// console.log('ddd');
